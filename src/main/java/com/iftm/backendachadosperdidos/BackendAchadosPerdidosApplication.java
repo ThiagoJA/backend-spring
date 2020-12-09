c5dfd2d3c7d13919f9a9ea1d485d66645c87b9c2
Thiago Jesuino <thiago_jesuino@hotmail.com>
2020-12-08 23:56:01 -0300

    First commit

---
+++ src/main/java/com/iftm/backendachadosperdidos/BackendAchadosPerdidosApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.backendachadosperdidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAchadosPerdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAchadosPerdidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/backendachadosperdidos/BackendAchadosPerdidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.backendachadosperdidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAchadosPerdidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

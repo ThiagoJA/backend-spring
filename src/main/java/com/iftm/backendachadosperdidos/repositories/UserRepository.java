9332fd48ad1bf8e5dfa9586b22e2fa515438fba6
Thiago Jesuino <thiago_jesuino@hotmail.com>
2021-01-09 01:07:07 -0300

    Custom user insert validation: unique email

--- src/main/java/com/iftm/backendachadosperdidos/repositories/UserRepository.java
+++ src/main/java/com/iftm/backendachadosperdidos/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.iftm.backendachadosperdidos.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+	User findByEmail(String email);
 }

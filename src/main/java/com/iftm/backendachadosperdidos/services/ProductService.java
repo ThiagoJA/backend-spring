75793f34e6f376a264ede2021d2f0f3cfa33765b
Thiago Jesuino <thiago_jesuino@hotmail.com>
2021-01-09 00:52:36 -0300

    Product insert

--- src/main/java/com/iftm/backendachadosperdidos/services/ProductService.java
+++ src/main/java/com/iftm/backendachadosperdidos/services/ProductService.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.iftm.backendachadosperdidos.dto.CategoryDTO;
+import com.iftm.backendachadosperdidos.dto.ProductCategoriesDTO;
 import com.iftm.backendachadosperdidos.dto.ProductDTO;
+import com.iftm.backendachadosperdidos.entities.Category;
 import com.iftm.backendachadosperdidos.entities.Product;
+import com.iftm.backendachadosperdidos.repositories.CategoryRepository;
 import com.iftm.backendachadosperdidos.repositories.ProductRepository;
 import com.iftm.backendachadosperdidos.resources.exeptions.ResourceNotFoundException;
 
@@ -18,6 +24,8 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	public List<ProductDTO> findAll() {
 		List<Product> list =  repository.findAll();
 		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
@@ -27,6 +35,22 @@ public class ProductService {
 		Optional<Product> obj = repository.findById(id);
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		for (CategoryDTO dto : categories) {
+			Category category = categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
 	}
 	
 }
